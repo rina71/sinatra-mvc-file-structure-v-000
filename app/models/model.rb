c1163d3e6692b1803ef46ba47bb9833fefb5c7e3
Yurina Shin <yurinas@gmail.com>
2018-08-06 17:41:01 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :age
   attr_reader :breed
 
-  def initilaize(name, breed, age)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
